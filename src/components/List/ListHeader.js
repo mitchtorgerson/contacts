10c79c1d24b47693996db63e73eab0723fb7db86
Mitch Torgerson <mitch.torgerson@twintechs.com>
2020-10-19 15:38:08 -0600

    Fixed the contact table

--- src/components/List/ListHeader.js
+++ src/components/List/ListHeader.js
@@ -9,6 +9,7 @@ function ListHeader({
         <div className={'list-header'}>
             {columns.map(item => (<div key={`${item.prop}`} className={'list-header-item'}>{item.title}</div>))}
             <div className={'list-header-item'}>&nbsp;</div>
+            <div className={'list-header-item'}>&nbsp;</div>
         </div>
     );
 }
